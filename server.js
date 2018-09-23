d717a9358cffdf6d71a4b6a9ea6c9c915aa22daa
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-09-22 17:04:29 -0700

    Create dockerfile that deploys proxy

--- server.js
+++ server.js
@@ -9,40 +9,47 @@ const fetch = require('node-fetch');
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// AVH
 app.get('/api/reviews/:productId', (req, res) => {
-  fetch(`http://ec2-54-193-46-143.us-west-1.compute.amazonaws.com/reviews/${req.params.productId}`)
+  fetch(`http://ec2-54-193-70-8.us-west-1.compute.amazonaws.com:80/reviews/${req.params.productId}`)
     .then((res) => {
       return res.json();
     })
     .then(json => res.send(json));
 });
 
+// AVH
 app.get('/api/helpful/:productId', (req, res) => {
-  fetch(`http://ec2-54-193-46-143.us-west-1.compute.amazonaws.com/helpful/${req.params.productId}`)
+  fetch(`http://ec2-54-193-70-8.us-west-1.compute.amazonaws.com:80/helpful/${req.params.productId}`)
     .then((res) => {
       // do nothing
     })
     .then(json => res.status(202).send());
 });
 
+// Matt
 app.get('/api/checkout/:productId', (req, res) => {
-  fetch(`http://localhost:7777/checkout/${req.params.productId}`)
+  fetch(`http://ec2-18-224-5-50.us-east-2.compute.amazonaws.com:80/checkout/${req.params.productId}`)
     .then((res) => {
       return res.json();
     })
     .then(json => res.send(json));
 });
 
+// Sonia
+// http://ec2-13-57-246-165.us-west-1.compute.amazonaws.com:3036/?id=1
 app.get('/api/relatedItems', (req, res) => {
-  fetch(`http://localhost:4043/product?id=${req.query.id}`)
+  fetch(`http://ec2-13-57-246-165.us-west-1.compute.amazonaws.com:3036/product?id=${req.query.id}`)
     .then((res) => {
       return res.json();
     })
     .then(json => res.send(JSON.stringify(json)));
 });
 
+// Michelle
+// http://ec2-13-57-32-246.us-west-1.compute.amazonaws.com/?id=2
 app.get('/api/products/', (req, res) => {
-  fetch(`http://localhost:9001/get?id=${req.query.id}`)
+  fetch(`http://ec2-13-57-32-246.us-west-1.compute.amazonaws.com:80/get?id=${req.query.id}`)
     .then(response => {
       return response.json()
     }).then(json => {
